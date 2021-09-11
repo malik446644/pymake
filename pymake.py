1d709afeddd55be7346f48f8cbfc431bf1424c97
MalikNajjar <59079190+malik446644@users.noreply.github.com>
2021-09-11 20:15:54 +0100

    made a lot of helpfull functions man

--- pymake.py
+++ pymake.py
@@ -1,35 +1,42 @@
 import sys
 import pymake as pm
 
-# creating two important files
-pm.mkdir("tmp")
-pm.mkdir("bin")
+if (len(sys.argv) == 1):
+    # creating two important files
+    print("\n>>>>>>>>>>>>    CREATING FOLDERS    <<<<<<<<<<<<")
+    pm.mkdir("tmp")
+    pm.mkdir("bin")
 
-# names of the source files
-src_names = ["main"]
+    # names of the source files
+    src_names = ["main"]
 
-# compiling the source files
-print(">>>>>>>>>>>>    COMPILING    <<<<<<<<<<<<")
-pm.fcmd(
-    "src/" + src_names[0] + ".c",
-    "tmp/" + src_names[0] + ".o", 
-    [
-        "gcc", 
-        "-c", 
-        "-o", 
-        "tmp/" + src_names[0] + ".o", 
-        "src/" + src_names[0] + ".c"
-    ]
-)
+    # compiling the source files
+    print("\n>>>>>>>>>>>>    COMPILING    <<<<<<<<<<<<")
+    pm.fcmd(
+        f"src/{src_names[0]}.c",
+        f"tmp/{src_names[0]}.o", 
+        [
+            "gcc", 
+            "-c", 
+            "-o", 
+            f"tmp/{src_names[0]}.o", 
+            f"src/{src_names[0]}.c"
+        ]
+    )
 
-# linking the object files
-print(">>>>>>>>>>>>    LINKING    <<<<<<<<<<<<")
-names_with_path = pm.add_str_to_arr_elements(src_names, "tmp/", ".o")
-pm.cmd(
-    [
-        "gcc", 
-        "-o", 
-        "bin/main.exe", 
-        " ".join(names_with_path)
-    ]
-)
+    # linking the object files
+    print("\n>>>>>>>>>>>>    LINKING    <<<<<<<<<<<<")
+    names_with_path = pm.add_str_to_arr_elements(src_names, "tmp/", ".o")
+    pm.cmd(
+        [
+            "gcc", 
+            "-o", 
+            "bin/app.exe", 
+            " ".join(names_with_path)
+        ]
+    )
+
+elif ("clean" in sys.argv):
+    print("\n>>>>>>>>>>>>    CLEANING    <<<<<<<<<<<<")
+    pm.rmdir("bin")
+    pm.rmdir("tmp")
--- pymake/__init__.py
+++ pymake/__init__.py
@@ -3,6 +3,8 @@ import os.path, time
 import subprocess
 
 def mkdir(new_directory_name):
+    """making a new directory"""
+
     # getting the project folder full path 
     current_directory = os.getcwd()
 
@@ -13,9 +15,11 @@ def mkdir(new_directory_name):
     try:
         os.mkdir(path)
     except:
-        print("[INFO] " + new_directory_name + " folder already exist")
+        print("[WARNING] " + new_directory_name + " folder already exist")
 
 def fcmd(source_file, dependant_file, cmd_arr):
+    """executing shell command if the dependant file is older than the source file"""
+
     # getting the project folder full path 
     current_directory = os.getcwd()
 
@@ -36,13 +40,44 @@ def fcmd(source_file, dependant_file, cmd_arr):
     if(source_last_modified > dependant_last_modified):
         cmd(cmd_arr)
     else:
-        print("[INFO] " + source_file + " file did not change")
+        print("[WARNING] " + source_file + " file did not change")
 
 def cmd(cmd_arr):
+    """executing shell command"""
+
     print(" ".join(cmd_arr))
     subprocess.run(cmd_arr, shell=True)
 
+def rmdir(folder_path):
+    """removes a directory"""
+
+    current_directory = os.getcwd()
+
+    if(os.path.isdir(folder_path) == False):
+        print("[WARNING] the directory <" + folder_path + "> does not exist!")
+        return
+
+    for root, dirs, files in os.walk(folder_path, topdown=False):
+        for name in files:
+            os.remove(os.path.join(root, name))
+        for name in dirs:
+            os.rmdir(os.path.join(root, name))
+
+    os.rmdir(os.path.join(current_directory, folder_path))
+    print("[info] successfully cleaned <" + folder_path + "> directory")
+
+def rmindir(folder_path):
+    """removing only the files and directories inside a directory"""
+
+    for root, dirs, files in os.walk(folder_path, topdown=False):
+        for name in files:
+            os.remove(os.path.join(root, name))
+        for name in dirs:
+            os.rmdir(os.path.join(root, name))
+    print("[info] successfully cleaned the files or folders inside <" + folder_path + "> directory")
+
 def add_str_to_arr_elements(arr, before_str, after_str):
+    """adding string before and after every string item in an array of strings"""
     arr2 = []
     for item in arr:
         arr2.append(before_str + item + after_str)
